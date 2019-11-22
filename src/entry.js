7f7d452c1ae03c5121e64c90a2190cf90682d45b
daoming.lzh <daoming.lzh@alibaba-inc.com>
2019-11-22 15:39:31 +0800

    add OrbitControls example

--- src/entry.js
+++ src/entry.js
@@ -8,19 +8,21 @@
  */
 
 import { WebGLRenderer, PerspectiveCamera, Scene, Vector3 } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import SeedScene from './objects/Scene.js';
 
 const scene = new Scene();
 const camera = new PerspectiveCamera();
-const renderer = new WebGLRenderer({antialias: true});
+const renderer = new WebGLRenderer({ antialias: true });
 const seedScene = new SeedScene();
+const orbitControls = new OrbitControls(camera);
 
 // scene
 scene.add(seedScene);
 
 // camera
-camera.position.set(6,3,-10);
-camera.lookAt(new Vector3(0,0,0));
+camera.position.set(6, 3, -10);
+camera.lookAt(new Vector3(0, 0, 0));
 
 // renderer
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -28,6 +30,7 @@ renderer.setClearColor(0x7ec0ee, 1);
 
 // render loop
 const onAnimationFrameHandler = (timeStamp) => {
+  orbitControls.update();
   renderer.render(scene, camera);
   seedScene.update && seedScene.update(timeStamp);
   window.requestAnimationFrame(onAnimationFrameHandler);
@@ -35,7 +38,7 @@ const onAnimationFrameHandler = (timeStamp) => {
 window.requestAnimationFrame(onAnimationFrameHandler);
 
 // resize
-const windowResizeHanlder = () => { 
+const windowResizeHanlder = () => {
   const { innerHeight, innerWidth } = window;
   renderer.setSize(innerWidth, innerHeight);
   camera.aspect = innerWidth / innerHeight;
@@ -46,5 +49,5 @@ window.addEventListener('resize', windowResizeHanlder);
 
 // dom
 document.body.style.margin = 0;
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
 
